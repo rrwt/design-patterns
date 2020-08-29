b6c75610698cd9ed1f008b7edb125bed45a29347
Rohit Rawat <rohit.rawat@presencelearning.com>
2020-08-29 12:51:45 -0500

    adapter pattern with examples. modified readme

---
+++ structural/adapter/__init__.py
@@ -0,0 +1,8 @@
+"""
+Adapter Design Pattern
+-------------------------------------------------------------
+A creational design pattern that allows objects with incompatible
+interfaces to collaborate. It has two implementations:
+    - Object Adapter
+    - Class Adapter
+"""
